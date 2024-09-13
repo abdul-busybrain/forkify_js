bd1f4d9162bc0edae8c399f7f023681ac0fcfb7b
abdul-busybrain <awwalhajine@gmail.com>
2024-09-13 14:16:36 +0100

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RESULT_PER_PAGE = 10;
+export const KEY = `e750da6c-651d-4957-8aa2-19e88f7c9037`;
+export const MODAL_CLOSE_SEC = 2.5;
